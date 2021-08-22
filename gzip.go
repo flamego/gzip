8bf08427421814ce3a957dbde6ed4c36b7c890db
Moesang <Moesang@users.noreply.github.com>
2021-08-23 00:10:56 +0800

    remove unused const

--- gzip.go
+++ gzip.go
@@ -14,7 +14,6 @@ import (
 const (
 	headerAcceptEncoding  = "Accept-Encoding"
 	headerContentEncoding = "Content-Encoding"
-	headerContentType     = "Content-Type"
 	headerVary            = "Vary"
 )
 
